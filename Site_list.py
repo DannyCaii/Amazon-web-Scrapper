61281b68178b00f0d034bea88bbc8379ded5fb99
Danny <dannycai1018@gmail.com>
2024-05-23 22:17:39 -0700

    Delete Site_list.py

--- Site_list.py
+++
@@ -1,8 +0,0 @@
-site_to_check = [
-    # Fresh Produce
-    'https://www.amazon.com/Organic-Honeycrisp-Apple-One-Medium/dp/B001GIP2A8/ref=sr_1_3_f3_wg?almBrandId=QW1hem9uIEZyZXNo&fpw=alm&s=amazonfresh&sr=1-3',
-    'https://www.amazon.com/Dole-Organic-Bananas-Bag/dp/B07ZLF9G83?pd_rd_i=B07ZLF9G83&fpw=alm&almBrandId=QW1hem9uIEZyZXNo&ref_=pd_alm_fs_dsk_sf_ai_16318981_1_1_i',
-    'https://www.amazon.com/produce-aisle-Strawberries-1-lb/dp/B000P6J0SM?pd_rd_i=B000P6J0SM&fpw=alm&almBrandId=QW1hem9uIEZyZXNo&ref_=pd_alm_fs_dsk_sf_ai_16318981_1_3_i',
-    'https://www.amazon.com/Shrimp-White-Farm-Raised-Frozen/dp/B07FZFB494?pd_rd_i=B07FZFB494&fpw=alm&almBrandId=VUZHIFdob2xlIEZvb2Rz&ref_=pd_alm_wf_dsk_dp_dzrp_1_6_i',
-    'https://www.amazon.com/Seafood-Halibut-Fillet-Msc/dp/B07HMC7VGJ?pd_rd_i=B07HMC7VGJ&fpw=alm&almBrandId=VUZHIFdob2xlIEZvb2Rz&ref_=pd_alm_wf_dsk_dp_dzrp_1_2_i'
-]
